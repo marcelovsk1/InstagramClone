8635292e36c042b1bfca34ab373897e8415186f1
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-23 23:40:07 -0400

    working on TabBar

---
+++ InstagramClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  InstagramClone
+//
+//  Created by Marcelo Amaral Alves on 2023-10-23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
