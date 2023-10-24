22e1ea25c7d8efbcfb2baf23c84f6fe0fa9d5751
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-24 06:02:10 -0400

    search view is done

--- InstagramClone/Core/Search/SearchView.swift
+++ InstagramClone/Core/Search/SearchView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct SearchView: View {
+    
+    @State private var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(0 ... 15, id: \.self) { user in
+                        HStack {
+                            Image("profilePicture1")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 40, height: 40)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading) {
+                                Text("lewishamilton")
+                                    .fontWeight(.semibold)
+                             
+                                Text("Lewis Hamilton")
+                                
+                            }
+                            .font(.footnote)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                .padding(.top, 8)
+                .searchable(text: $searchText, prompt: "Search")
+            }
+            .navigationTitle("Explore")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
