10b4c0b8a8643783c9da39a16681e96a6ac4cb84
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-23 22:24:04 -0400

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Marcelo Amaral Alves on 2023-10-23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
