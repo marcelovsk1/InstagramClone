a6dbfa2b6382a47da1d398c83edf879a4a3b3a65
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-24 04:18:28 -0400

    start to working on feed

---
+++ InstagramClone/Core/Feed/FeedCall.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCall.swift
+//  InstagramClone
+//
+//  Created by Marcelo Amaral Alves on 2023-10-24.
+//
+
+import SwiftUI
+
+struct FeedCall: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCall()
+}
