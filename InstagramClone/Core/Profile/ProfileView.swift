6a263114563bf4e5eae4490ac00298f92bc83f3e
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-23 23:42:03 -0400

    working on ProfileView

---
+++ InstagramClone/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  InstagramClone
+//
+//  Created by Marcelo Amaral Alves on 2023-10-23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
