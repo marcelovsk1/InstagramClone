ede7cf6a2e1e38a2c033d83ab606972ddeb66366
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-24 17:52:52 -0400

    create usernameview is done

---
+++ InstagramClone/Core/Authentication/CreateUserNameView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateUserNameView.swift
+//  InstagramClone
+//
+//  Created by Marcelo Amaral Alves on 2023-10-24.
+//
+
+import SwiftUI
+
+struct CreateUserNameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateUserNameView()
+}
