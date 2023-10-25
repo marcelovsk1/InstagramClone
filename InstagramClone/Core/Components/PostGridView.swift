cb2cab9cb9ce2cfc34407ce5e37e359e16b4ae4c
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-25 12:08:47 -0400

    creating post grid view

---
+++ InstagramClone/Core/Components/PostGridView.swift
@@ -0,0 +1,18 @@
+//
+//  PostGridView.swift
+//  InstagramClone
+//
+//  Created by Marcelo Amaral Alves on 2023-10-25.
+//
+
+import SwiftUI
+
+struct PostGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostGridView()
+}
