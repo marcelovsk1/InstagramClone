88a567999aabd09aa0cebcaa37d27900cfe76ea5
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-25 23:33:32 -0400

    creating the profile header view

---
+++ InstagramClone/Core/Profile/ProfileHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileHeaderView.swift
+//  InstagramClone
+//
+//  Created by Marcelo Amaral Alves on 2023-10-25.
+//
+
+import SwiftUI
+
+struct ProfileHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileHeaderView()
+}
