6aaffdde66eba0148a688a90decc9dec2cfa0c82
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-25 23:47:40 -0400

    creating uploadpostview

---
+++ InstagramClone/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,18 @@
+//
+//  UploadPostView.swift
+//  InstagramClone
+//
+//  Created by Marcelo Amaral Alves on 2023-10-25.
+//
+
+import SwiftUI
+
+struct UploadPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadPostView()
+}
