25dbfedeeae5e1ee57621996bd48a3d85f3f13a2
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-07 13:27:32 -0300

    fixing some bugs

--- InstagramClone/Core/Root/ViewModel/ContentViewModel.swift
+++ InstagramClone/Core/Root/ViewModel/ContentViewModel.swift
@@ -16,14 +16,20 @@ class ContentViewModel: ObservableObject {
     private var cancellables = Set<AnyCancellable>()
     
     @Published var userSession: FirebaseAuth.User?
+    @Published var currentUser: User?
     
     init() {
-        
+       setupSubscribers()
     }
     
     func setupSubscribers() {
         service.$userSession.sink { [weak self] userSession in
             self?.userSession = userSession
+        }
+        .store(in: &cancellables)
+        
+        service.$currentUser.sink { [weak self ] currentUser in
+            self?.currentUser = currentUser
         }
         .store(in: &cancellables)
     }
